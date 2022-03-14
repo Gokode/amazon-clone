8adaaa90db675289946fc2dc0483592947badcb2
Gokul G <gokulg1549@gmail.com>
2022-03-15 01:16:16 +0530

    after checkout page

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -3,9 +3,10 @@ import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format"
 import { useStateValue } from "./StateProvider";
 import { getBasketTotal } from "./reducer";
-
+import { useNavigate} from "react-router-dom";
 
 function Subtotal() {
+  const navigate  = useNavigate();
   const[{basket}, dispatch] = useStateValue();
   return (
     <div className="subtotal">
@@ -29,7 +30,7 @@ function Subtotal() {
              prefix={"₹"}
          />
 
-         <button>Proceed to checkout</button>
+         <button onClick={e => navigate('/payment')}>Proceed to checkout</button>
     </div>
   )
 }
